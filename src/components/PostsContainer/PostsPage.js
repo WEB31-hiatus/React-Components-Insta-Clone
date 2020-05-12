dc8dce32c1d9b4595288bb688aa6ac6d17f6ae60
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-05-12 16:13:29 -0700

    initial commit

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,11 +1,14 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
+import PostHeader from './PostHeader';
+import CommentSection from '../CommentSection/CommentSectionContainer';
+import LikeSection from './LikeSection'
 // import data 
 
-const PostsPage = () => {
+const PostsPage = (props) => {
   // set up state for your data
   return (
     <div className="posts-container-wrapper">
