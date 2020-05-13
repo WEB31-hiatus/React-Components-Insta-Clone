067b27fede57d9d1006eb8dfe1af71cce7d43fc1
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-05-13 14:45:02 -0700

    postsPage.js done

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -3,16 +3,18 @@
 import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
-import PostHeader from './PostHeader';
-import CommentSection from '../CommentSection/CommentSectionContainer';
-import LikeSection from './LikeSection'
+import dummydata from "../../dummy-data"
 // import data 
 
 const PostsPage = (props) => {
   // set up state for your data
+  const [data] = useState(dummydata);
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {data.map(item => ( 
+        <Post post = {item} />
+      ))};
     </div>
   );
 };
